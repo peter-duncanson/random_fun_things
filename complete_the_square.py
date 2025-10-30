b20205d03397801742d895919d714c976346a570
peter-duncanson <85356486+peter-duncanson@users.noreply.github.com>
2025-10-30 17:36:48 -0400

    Add files via upload

---
+++ complete_the_square.py
@@ -0,0 +1,13 @@
+def complete_square(b, c):
+
+    if (-c + (b/2)**2) > 0:
+        std = ('(x + {:.2f})^2 = {:.2f}'.format(b/2, -c + ((b/2)**2)))
+        roots = 'Roots: {:.2f} and {:.2f}'.format(-b/2 + (-c + ((b/2)**2))**0.5, -b/2 - (-c + ((b/2)**2))**0.5)
+        return std, roots
+    
+    else:
+        return 'imaginary roots, add soon'
+
+
+print(complete_square(2, -3767676))
+
